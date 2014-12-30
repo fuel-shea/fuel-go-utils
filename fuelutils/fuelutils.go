df7c2b1166a5710ca0abff6208d62cbec18a3646
Shea Clare <shea@fuelpowered.com>
2014-12-30 13:06:30 -0800

    Add err return val from utils.InterfaceArr2...

--- fuelutils/fuelutils.go
+++ fuelutils/fuelutils.go
@@ -1,10 +1,19 @@
 package fuelutils
 
-func InterfaceArr2StringArr(ifArray []interface{}) []string {
+import (
+	"errors"
+	"fmt"
+)
+
+func InterfaceArr2StringArr(ifArray []interface{}) ([]string, error) {
 	nElems := len(ifArray)
 	strs := make([]string, nElems)
 	for i := 0; i < nElems; i++ {
-		strs[i] = ifArray[i].(string)
+		var isString bool
+		strs[i], isString = ifArray[i].(string)
+		if !isString {
+			return []string{}, errors.New(fmt.Sprintf("Tried to convert %v to string", ifArray[i]))
+		}
 	}
-	return strs
+	return strs, nil
 }
--- fuelutils/fuelutils_test.go
+++ fuelutils/fuelutils_test.go
@@ -6,17 +6,22 @@ import (
 	"testing"
 )
 
-func TestInterfaceArr2StringArr(t *testing.T) {
+func TestInterfaceArr2StringArr_success(t *testing.T) {
 	interfaceArr := []interface{}{
 		"str1",
 		"str2",
 		"str3",
 	}
 
-	strArr := fuelutils.InterfaceArr2StringArr(interfaceArr)
+	strArr, err := fuelutils.InterfaceArr2StringArr(interfaceArr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	if len(interfaceArr) != len(strArr) {
 		t.Fatal("The resulting array is", len(strArr), "elements long, but it should be", len(interfaceArr))
 	}
+
 	for interfaceIdx, interfaceElem := range interfaceArr {
 		strElem := strArr[interfaceIdx]
 		if reflect.TypeOf(strElem).Kind() != reflect.String {
@@ -25,5 +30,18 @@ func TestInterfaceArr2StringArr(t *testing.T) {
 		if interfaceElem.(string) != strElem {
 			t.Error("Element", interfaceIdx, "in converted array should be", interfaceElem, "but is", strElem)
 		}
+	}
+}
+
+func TestInterfaceArr2StringArr_failure(t *testing.T) {
+	interfaceArr := []interface{}{
+		"str1",
+		"str2",
+		13, // not a string!!! D:
+	}
+
+	_, err := fuelutils.InterfaceArr2StringArr(interfaceArr)
+	if err == nil {
+		t.Error("Expecting error to result, but it did not")
 	}
 }
